a889100184aac15e38d27df809fa28074e2a4578
Vladislav <ivaskevichvladislav@gmail.com>
2021-03-03 23:59:02 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if(Array.isArray(array) && array.length) {
+return Math.min.apply(null, array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(Array.isArray(array) && array.length) {
+  return Math.max.apply(null, array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(Array.isArray(array) && array.length) {
+  return array.reduce((a, b) => (a + b)) / array.length;
+  }
   return 0;
 }
